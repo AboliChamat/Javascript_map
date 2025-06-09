e9f67a251a819122c201c8069634b95b300251d9
Aboli <abolichamat17@gmail.com>
2025-06-09 20:04:21 +0530

    Vanilla js

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-console.log("Hello World");
+// How can we write Vanilla js
+
+// Type 1|| Inline styles via element.style
+const button=document.querySelector('.my-button');
+button.style.backgroundColor='blue';
+button.style.color='pink';
+button.style.padding='10px 20px';
+
+// Type 2|| Dynamically injecting global styles
+const style=document.createElement('style');
+style.innerHTML=`
+.my-button{
+background-color:blue;
+color:pink;
+padding:10px 20px
+}
+`;
+document.head.appendChild(style);
+
+// Type 3|| Modifying Classes with classList
+const button=document.querySelector('.my-button');
+button.classList.add('active');
+button.classList.remove('inactive');
+button.classList.toggle('highlisted');
+
+
+
