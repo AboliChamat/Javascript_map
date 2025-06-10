8d5c9c8bc9d6cbb3bc23b475157bb2b2e7624974
Aboli <abolichamat17@gmail.com>
2025-06-10 18:03:29 +0530

    Map Method

---
+++ Array Methods.js
@@ -0,0 +1,14 @@
+// Array Methods
+// Map Method
+let something=[1,2,3,4,5];
+let Map=something.map((x)=>x*2);
+console.log(Aboli);
+
+// Filter Method 
+let replaceItem=5;
+let Filter=something.filter((x)=>x!=replaceItem);
+console.log(Filter);
+
+// Reduce Method 
+let Reduce=something.reduce((x,a)=>x+a);
+console.log(Reduce);
